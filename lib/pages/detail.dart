7e3af05ad267be5bf048a05a4595d3f31b8c0379
abdulbasitsaid <mrspeed518@gmail.com>
2020-06-09 19:55:56 +0100

    swiping added

--- lib/pages/detail.dart
+++ lib/pages/detail.dart
@@ -9,77 +9,83 @@ class DetialPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Stack(
-          children: <Widget>[
-            Container(
-              height: SizeConfig.h,
-              width: SizeConfig.w,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: AssetImage('${birthdayCardModel.imageUrl}'),
-                ),
-              ),
-            ),
-            //text
-            Positioned(
-              bottom: 0,
-              child: Container(
-                padding: EdgeInsets.all(SizeConfig.sizeMultiplier),
+        child: GestureDetector(
+          onPanUpdate: (drage) {
+            if (drage.delta.dx > 0) {
+              Navigator.pop(context);
+            }
+          },
+          child: Stack(
+            children: <Widget>[
+              Container(
+                height: SizeConfig.h,
+                width: SizeConfig.w,
                 decoration: BoxDecoration(
-                  color: Colors.black.withOpacity(0.8),
-                ),
-                child: Column(
-                  children: <Widget>[
-                    Container(
-                      width: SizeConfig.widthMultiplier * 80,
-                      child: Text(
-                        '${birthdayCardModel.title}',
-                        softWrap: true,
-                        style: Theme.of(context)
-                            .textTheme
-                            .headline6
-                            .copyWith(color: Colors.white),
-                      ),
-                    ),
-                    SizedBox(
-                      height: SizeConfig.heightMultiplier,
-                    ),
-                    Container(
-                      width: SizeConfig.widthMultiplier * 80,
-                      child: Text(
-                        '${birthdayCardModel.content}',
-                        softWrap: true,
-                        style: Theme.of(context)
-                            .textTheme
-                            .bodyText1
-                            .copyWith(color: Colors.white),
-                      ),
-                    ),
-                  ],
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage('${birthdayCardModel.imageUrl}'),
+                  ),
                 ),
               ),
-            ),
-            Positioned(
-              child: GestureDetector(
-                onHorizontalDragStart: (_) => Navigator.pop(context),
-                onTap: () => Navigator.pop(context),
+              //text
+              Positioned(
+                bottom: 0,
                 child: Container(
-                  color: Colors.black.withOpacity(0.4),
                   padding: EdgeInsets.all(SizeConfig.sizeMultiplier),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  decoration: BoxDecoration(
+                    color: Colors.black.withOpacity(0.8),
+                  ),
+                  child: Column(
                     children: <Widget>[
-                      Icon(
-                        Icons.arrow_back,
-                        color: Colors.white,
-                      )
+                      Container(
+                        width: SizeConfig.widthMultiplier * 80,
+                        child: Text(
+                          '${birthdayCardModel.title}',
+                          softWrap: true,
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline6
+                              .copyWith(color: Colors.white),
+                        ),
+                      ),
+                      SizedBox(
+                        height: SizeConfig.heightMultiplier,
+                      ),
+                      Container(
+                        width: SizeConfig.widthMultiplier * 80,
+                        child: Text(
+                          '${birthdayCardModel.content}',
+                          softWrap: true,
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyText1
+                              .copyWith(color: Colors.white),
+                        ),
+                      ),
                     ],
                   ),
                 ),
               ),
-            )
-          ],
+              Positioned(
+                child: GestureDetector(
+                  onTap: () => Navigator.pop(context),
+                  child: Container(
+                    color: Colors.black.withOpacity(0.4),
+                    padding: EdgeInsets.all(SizeConfig.sizeMultiplier),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Icon(
+                          Icons.arrow_back,
+                          color: Colors.white,
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
