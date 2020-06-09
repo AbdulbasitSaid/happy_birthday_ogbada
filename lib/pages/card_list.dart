cb8b399068a0b00eae3f19983cc20862185b732b
abdulbasitsaid <mrspeed518@gmail.com>
2020-06-09 19:27:26 +0100

    added splash

--- lib/pages/card_list.dart
+++ lib/pages/card_list.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:happy_birthday/pages/detail.dart';
 import 'package:happy_birthday/providers/cards.dart';
 import 'package:happy_birthday/utils/screen_util.dart';
 import 'package:provider/provider.dart';
@@ -14,11 +16,14 @@ class CardListPage extends StatefulWidget {
 class _CardListPageState extends State<CardListPage> {
   ScrollController controller = ScrollController();
   bool closeTopScroller = false;
+  double topContainer = 0;
   @override
   void initState() {
     controller.addListener(() {
+      double value = controller.offset / 50;
       setState(() {
         closeTopScroller = controller.offset > 50;
+        topContainer = value;
       });
     });
     super.initState();
@@ -38,77 +43,154 @@ class _CardListPageState extends State<CardListPage> {
               children: <Widget>[
                 //firstScroll
                 AnimatedContainer(
-                  duration: Duration(microseconds: 200),
+                  duration: const Duration(milliseconds: 1000),
                   width: SizeConfig.w,
                   alignment: Alignment.topCenter,
                   height:
                       closeTopScroller ? 0 : SizeConfig.heightMultiplier * 20,
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: value.cards.length,
-                    itemBuilder: (context, index) => Container(
-                      width: SizeConfig.widthMultiplier * 30,
-                      child: Stack(
-                        children: <Widget>[
-                          Container(
-                            margin: EdgeInsets.all(SizeConfig.sizeMultiplier),
-                            height: SizeConfig.h,
-                            width: SizeConfig.w,
-                            decoration: BoxDecoration(
-                              image: DecorationImage(
-                                  fit: BoxFit.cover,
-                                  image:
-                                      AssetImage(value.cards[index].imageUrl)),
-                            ),
-                          ),
-                          Container(
-                            color: Colors.grey.shade900.withOpacity(0.3),
-                            padding:
-                                EdgeInsets.all(SizeConfig.sizeMultiplier * 2),
-                            width: SizeConfig.widthMultiplier * 30,
-                            child: Text('${value.cards[index].title}',
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .bodyText1
-                                    .copyWith(
-                                      color: Colors.white,
-                                    )),
-                          ),
-                          Positioned(
-                            bottom: 0,
-                            right: 0,
-                            child: Icon(
-                              Icons.play_arrow,
-                              size: SizeConfig.sizeMultiplier * 6,
-                              color: Colors.red,
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: SizeConfig.heightMultiplier * 2,
-                ),
-                Expanded(
                   child: Container(
-                    height: SizeConfig.heightMultiplier * 20,
                     child: ListView.builder(
-                        controller: controller,
-                        scrollDirection: Axis.vertical,
-                        itemCount: value.cards.length,
-                        itemBuilder: (context, index) => Container(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: value.cards.length,
+                      itemBuilder: (context, index) => Container(
+                        width: SizeConfig.widthMultiplier * 30,
+                        child: Stack(
+                          children: <Widget>[
+                            Container(
                               margin: EdgeInsets.all(SizeConfig.sizeMultiplier),
-                              height: SizeConfig.heightMultiplier * 30,
-                              width: SizeConfig.widthMultiplier * 30,
+                              height: SizeConfig.h,
+                              width: SizeConfig.w,
                               decoration: BoxDecoration(
                                 image: DecorationImage(
                                     fit: BoxFit.cover,
                                     image: AssetImage(
                                         value.cards[index].imageUrl)),
                               ),
-                            )),
+                            ),
+                            Container(
+                              color: Colors.grey.shade900.withOpacity(0.3),
+                              padding:
+                                  EdgeInsets.all(SizeConfig.sizeMultiplier * 2),
+                              width: SizeConfig.widthMultiplier * 30,
+                              child: Text('${value.cards[index].title}',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .bodyText1
+                                      .copyWith(
+                                        color: Colors.white,
+                                      )),
+                            ),
+                            Positioned(
+                              bottom: 0,
+                              right: 0,
+                              child: Icon(
+                                MaterialCommunityIcons.heart,
+                                size: SizeConfig.sizeMultiplier * 5,
+                                color: Colors.pink,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: SizeConfig.heightMultiplier * 3,
+                ),
+                Expanded(
+                  child: Container(
+                    child: ListView.builder(
+                        controller: controller,
+                        scrollDirection: Axis.vertical,
+                        itemCount: value.cards.length,
+                        itemBuilder: (context, index) {
+                          double scale = 1.0;
+                          if (topContainer > 0.3) {
+                            scale = index + 0.3 - topContainer;
+                            if (scale < 0) {
+                              scale = 0;
+                            } else if (scale > 1) {
+                              scale = 1;
+                            }
+                          }
+                          return Opacity(
+                            opacity: scale,
+                            child: Transform(
+                              transform: Matrix4.identity()
+                                ..scale(scale, scale),
+                              alignment: Alignment.topCenter,
+                              child: Align(
+                                  heightFactor: .3,
+                                  alignment: Alignment.topCenter,
+                                  child: GestureDetector(
+                                    onTap: () => Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (_) => DetialPage(
+                                                  birthdayCardModel:
+                                                      value.cards[index],
+                                                ))),
+                                    child: Container(
+                                      height: SizeConfig.heightMultiplier * 60,
+                                      width: SizeConfig.widthMultiplier * 95,
+                                      child: Stack(
+                                        children: <Widget>[
+                                          Container(
+                                            margin: EdgeInsets.all(
+                                              SizeConfig.sizeMultiplier,
+                                            ),
+                                            decoration: BoxDecoration(
+                                              boxShadow: [
+                                                BoxShadow(
+                                                    offset: Offset(0, 0),
+                                                    color: Colors.black12,
+                                                    spreadRadius: 4,
+                                                    blurRadius: 4)
+                                              ],
+                                              borderRadius:
+                                                  BorderRadius.circular(
+                                                SizeConfig.sizeMultiplier,
+                                              ),
+                                              image: DecorationImage(
+                                                  fit: BoxFit.cover,
+                                                  image: AssetImage(value
+                                                      .cards[index].imageUrl)),
+                                            ),
+                                          ),
+                                          Container(
+                                            // color: Color(0xffc21e56),
+                                            color: Colors.pink,
+                                            width:
+                                                SizeConfig.widthMultiplier * 60,
+                                            padding: EdgeInsets.all(
+                                              SizeConfig.sizeMultiplier * 2,
+                                            ),
+                                            child: Text(
+                                              '${value.cards[index].title}',
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .headline6
+                                                  .copyWith(
+                                                      color: Colors.white,
+                                                      fontSize: 16),
+                                            ),
+                                          ),
+                                          Align(
+                                            alignment: Alignment.topRight,
+                                            child: Container(
+                                                padding: EdgeInsets.all(
+                                                    SizeConfig.sizeMultiplier),
+                                                color: Colors.white,
+                                                child: Text('Tap to read...')),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  )),
+                            ),
+                          );
+                        }),
                   ),
                 )
               ],
---
+++ lib/pages/detail.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:happy_birthday/models/birthday_card.dart';
+import 'package:happy_birthday/utils/screen_util.dart';
+
+class DetialPage extends StatelessWidget {
+  const DetialPage({Key key, this.birthdayCardModel}) : super(key: key);
+  final BirthdayCardModel birthdayCardModel;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: <Widget>[
+            Container(
+              height: SizeConfig.h,
+              width: SizeConfig.w,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage('${birthdayCardModel.imageUrl}'),
+                ),
+              ),
+            ),
+            //text
+            Positioned(
+              bottom: 0,
+              child: Container(
+                padding: EdgeInsets.all(SizeConfig.sizeMultiplier),
+                decoration: BoxDecoration(
+                  color: Colors.black.withOpacity(0.8),
+                ),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      width: SizeConfig.widthMultiplier * 80,
+                      child: Text(
+                        '${birthdayCardModel.title}',
+                        softWrap: true,
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline6
+                            .copyWith(color: Colors.white),
+                      ),
+                    ),
+                    SizedBox(
+                      height: SizeConfig.heightMultiplier,
+                    ),
+                    Container(
+                      width: SizeConfig.widthMultiplier * 80,
+                      child: Text(
+                        '${birthdayCardModel.content}',
+                        softWrap: true,
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyText1
+                            .copyWith(color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              child: GestureDetector(
+                onTap: () => Navigator.pop(context),
+                child: Container(
+                  color: Colors.black.withOpacity(0.4),
+                  padding: EdgeInsets.all(SizeConfig.sizeMultiplier),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Icon(
+                        Icons.arrow_back,
+                        color: Colors.white,
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
