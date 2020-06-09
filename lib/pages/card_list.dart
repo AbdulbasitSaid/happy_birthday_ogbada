3b4e8343ad8f7ba0d11e8d67bf825caf3b3012b7
abdulbasitsaid <mrspeed518@gmail.com>
2020-06-09 17:12:47 +0100

    adding animation

--- lib/pages/card_list.dart
+++ lib/pages/card_list.dart
@@ -12,6 +12,18 @@ class CardListPage extends StatefulWidget {
 }
 
 class _CardListPageState extends State<CardListPage> {
+  ScrollController controller = ScrollController();
+  bool closeTopScroller = false;
+  @override
+  void initState() {
+    controller.addListener(() {
+      setState(() {
+        closeTopScroller = controller.offset > 50;
+      });
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,33 +33,60 @@ class _CardListPageState extends State<CardListPage> {
       body: Consumer<CardsProvider>(
         builder: (BuildContext context, CardsProvider value, Widget child) {
           return Container(
-            // height: SizeConfig.h,
+            height: SizeConfig.h,
             child: Column(
               children: <Widget>[
-                Container(
-                  height: SizeConfig.heightMultiplier * 20,
+                //firstScroll
+                AnimatedContainer(
+                  duration: Duration(microseconds: 200),
+                  width: SizeConfig.w,
+                  alignment: Alignment.topCenter,
+                  height:
+                      closeTopScroller ? 0 : SizeConfig.heightMultiplier * 20,
                   child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: value.cards.length,
-                      itemBuilder: (context, index) => Container(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: value.cards.length,
+                    itemBuilder: (context, index) => Container(
+                      width: SizeConfig.widthMultiplier * 30,
+                      child: Stack(
+                        children: <Widget>[
+                          Container(
                             margin: EdgeInsets.all(SizeConfig.sizeMultiplier),
-                            height: SizeConfig.heightMultiplier * 5,
-                            width: SizeConfig.widthMultiplier * 30,
+                            height: SizeConfig.h,
+                            width: SizeConfig.w,
                             decoration: BoxDecoration(
                               image: DecorationImage(
                                   fit: BoxFit.cover,
                                   image:
                                       AssetImage(value.cards[index].imageUrl)),
                             ),
-                          )),
+                          ),
+                          Container(
+                            color: Colors.grey.shade900.withOpacity(0.3),
+                            padding:
+                                EdgeInsets.all(SizeConfig.sizeMultiplier * 2),
+                            width: SizeConfig.widthMultiplier * 30,
+                            child: Text('${value.cards[index].title}',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyText1
+                                    .copyWith(
+                                      color: Colors.white,
+                                    )),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
                 ),
                 SizedBox(
-                  height: SizeConfig.heightMultiplier * 6,
+                  height: SizeConfig.heightMultiplier * 2,
                 ),
                 Expanded(
                   child: Container(
                     height: SizeConfig.heightMultiplier * 20,
                     child: ListView.builder(
+                        controller: controller,
                         scrollDirection: Axis.vertical,
                         itemCount: value.cards.length,
                         itemBuilder: (context, index) => Container(
